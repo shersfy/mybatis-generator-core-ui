0213ee2755bf425030658ecd7edbdf33e4a148bd
shersfy <shersfy@163.com>
2020-04-17 23:46:47 +0800

    protected

--- src/main/resources/template/BaseServiceImpl.java
+++ src/main/resources/template/BaseServiceImpl.java
@@ -22,7 +22,7 @@ import com.alibaba.fastjson.JSON;
 public abstract class BaseServiceImpl<T extends BaseEntity, Id extends Serializable> 
 	implements BaseService<T, Id> {
 
-	private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
 	public abstract BaseMapper<T, Id> getMapper();
 
